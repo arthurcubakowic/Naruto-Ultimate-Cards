0702e2ac475f5156b29cd887be930d0f56cd116a
Arthur Cubakowic <arthur.cubakowic@aluno.ufabc.edu.br>
2021-04-22 01:22:14 -0300

    Torre manager comentado

--- Assets/Script/Torre/TorreManager.cs
+++ Assets/Script/Torre/TorreManager.cs
@@ -8,14 +8,14 @@ using UnityEngine.UI;
 public class TorreManager : MonoBehaviour
 {
     [Header("Geral")]
-    // Instancia referentes à torre
+    // Instancias referentes à torre
     public List<Torre> listaTorres;
     public Torre torre;
     public int andar;
-    // Instancia referentes ao jogador
+    // Instancias referentes ao jogador
     public Time time;
     public InventarioCenaManager inventarioManager;
-    // Instancia referente à musica de fundo
+    // Instancias referente à musica de fundo
     public AudioClip musicaThema;
 
     [Header("UI")]
@@ -50,60 +50,64 @@ public class TorreManager : MonoBehaviour
     // Variaveis locais, algumas usadas em Torre Botoes Manager
     public bool torreEmAndamento;
     public bool torreSelectEmAndamento;
-    public int i;
 
     private void Start()
     {
+        // Garante que a musica comece tocando
         gameObject.GetComponent<AudioSource>().clip = musicaThema;
         gameObject.GetComponent<AudioSource>().Play();
 
-        i = 0;
-
+        // variaveis para travar as teclas
         torreEmAndamento = false;
         torreSelectEmAndamento = true;
 
+        // Garante que a UI de jogador nao apareca na tela até o inicio da torre
         inimigoUI.SetActive(false);
         playerUI.SetActive(false);
 
+        // Comeca a rotina de selecao de torre
         StartCoroutine(SelecionaTorre());
 
     }
 
     private void Update()
     {
+        // se o jogador perdeu toda a vida e a torre ja comecou ele morreu, em seguida chama a rotina de GameOver
         if (vidaPlayer <= 0 && torreEmAndamento && !(andar == torre.listaTorre.Count))
         {
             torreEmAndamento = false;
             StartCoroutine(GameOver());
         }
 
+        // se o inimigo perdeu toda a vida ele é derrotado, e voce avanca para o proximo andar da torre
         if (vidaInimigo <= 0 && torreEmAndamento && !(andar == torre.listaTorre.Count))
         {
-            FindObjectOfType<AudioManager>().Stop("Ataque Sound");
+            FindObjectOfType<AudioManager>().Stop("Ataque Sound"); // Para o som do ataque final para dar lugar ao som de inimigo derrotado
             FindObjectOfType<AudioManager>().Play("Inimigo Derrotado");
             ProximoAndar();
         }
 
     }
 
-    //FALTA IMPLEMENTAR TUDO
-
+    // Rotina de selecao de torre
     public IEnumerator SelecionaTorre()
     {
-        int i = 0;
-        textoMensagem.text = "Aperte Enter para selecionar a torre";
+        int i = 0;  // Garante que o a lista de torres comece da primeira torre
+        textoMensagem.text = "Aperte Enter para selecionar a torre"; // muda o texto visivel para o jogador
 
+        // inicia a UI de selecao de torre
         torre = listaTorres[i];
         torreImage.sprite = listaTorres[i].imagemTorre;
         torreNome.text = listaTorres[i].nome;
 
+        // While que prende o jogador na selecao de torre até que uma torre seja selecionada, por isso esta funcao é uma rotina
         while (torreSelectEmAndamento)
         {
+            // use a seta da direita ou da esquerda para selecionar a torre que deseja jogar
             if (Input.GetKeyDown(KeyCode.RightArrow) && gameObject.GetComponent<TorreManager>().torreSelectEmAndamento)
             {
                 i++;
                 if (i >= listaTorres.Count) i = 0;
-                Debug.Log(i);
             }
 
             if (Input.GetKeyDown(KeyCode.LeftArrow) && gameObject.GetComponent<TorreManager>().torreSelectEmAndamento)
@@ -112,116 +116,65 @@ public class TorreManager : MonoBehaviour
                 if (i < 0) i = listaTorres.Count - 1;
             }
 
+            // use o Enter para selecionar a torre que deseja jogar
             if (Input.GetKeyDown(KeyCode.Return) && gameObject.GetComponent<TorreManager>().torreSelectEmAndamento)
             {
                 gameObject.GetComponent<TorreManager>().torreSelectEmAndamento = false;
                 FindObjectOfType<AudioManager>().Play("Select Carta");
             }
 
-
+            // sempre atualiza a sprite quando o i é alterado independente da quantidade de torres que existam no jogo
             torreImage.sprite = listaTorres[i].imagemTorre;
             torreNome.text = listaTorres[i].nome;
 
             yield return null;
         }
 
-        textoMensagem.text = "Torre Selecionada!";
 
-        torre = listaTorres[i];
+        textoMensagem.text = "Torre Selecionada!"; // muda o texto visivel para o jogador
 
-        yield return new WaitForSecondsRealtime(1);
-        torreSelect.SetActive(false);
-        StartCoroutine(FormarTime());
-    }
-    public IEnumerator GameOver()
-    {
-        Debug.Log("Voce Perdeu!");
 
-        gameObject.GetComponent<AudioSource>().Stop();
-        FindObjectOfType<AudioManager>().Play("Lose Theme");
+        torre = listaTorres[i];                    // atribui que a torre iniciada será a escolhida pelo jogador
 
-        caixaMensagem.SetActive(true);
-        caixaMensagem.transform.position = GameObject.Find("Canvas").transform.position;
-        textoMensagem.text = "Voce Perdeu! Volte Tudo";
-        
-        torreEmAndamento = false;
-
-        bool continueGame = false;
-        while (!continueGame)
-        {
-            if (Input.anyKeyDown)
-            {
-                continueGame = true;
-                FindObjectOfType<AudioManager>().Stop("Lose Theme");
-            }
-
-            yield return null;
-        }
-        MenuManager.LoadMenuScene();
+        yield return new WaitForSecondsRealtime(1);// espera um segundo para o jogador poder ler que a torre foi selecionada
+        torreSelect.SetActive(false);              // desativa a UI de selecao de torre
+        StartCoroutine(FormarTime());              // inicia a rotina de formacao de time
     }
 
-    public IEnumerator Vitoria()
-    {
-        Debug.Log("Parabens Voce Completou a Torre");
-
-        gameObject.GetComponent<AudioSource>().Stop();
-        FindObjectOfType<AudioManager>().Play("Vitoria Theme");
-
-        caixaMensagem.SetActive(true);
-        caixaMensagem.transform.position = GameObject.Find("Canvas").transform.position;
-        textoMensagem.text = "Parabens, Voce Completou a Torre!";
-
-        torreEmAndamento = false;
-        RecompensaJogador();
-
-        bool continueGame = false;
-        while (!continueGame)
-        {
-            if (Input.anyKeyDown)
-            {
-                continueGame = true;
-                FindObjectOfType<AudioManager>().Stop("Vitoria Theme");
-            }
-
-            yield return null;
-        }
-        MenuManager.LoadMenuScene();
-    }
-
-    public void RecompensaJogador()
-    {
-        GameManager.AplicaRecompensa(torre.recompensa);
-    }
-
+    // Rotina de formacao de time
     public IEnumerator FormarTime()
     {
-        int i = 1;
-        bool recebeuCarta = false;
+        int i = 1;                  // variavel para contar quantos membros do time foram escolgidos
+        bool recebeuCarta = false;  // variavel para travar o jogador na selecao de carta
 
+        // instancia a UI de cartas, para que as cartas fiquem visiveis para o jogador escolhe-las
         gameObject.GetComponent<TorreManager>().inventarioManager.InstanciaCarta(Inventory.playerData.inventarioCartas[gameObject.GetComponent<TorreManager>().inventarioManager.i]);
         gameObject.GetComponent<TorreManager>().inventarioManager.mostrandoInventario = true;
 
 
+        // Enquanto o jogador nao escolher uma carta ele fica preso na selecao de capitao
         while (!recebeuCarta)
         {
-            textoMensagem.text = "Escolha o capitao da sua equipe";
-            // quando o jogador apertar Espaco a carta que instanciada é selecionada para ser o capitao
+            textoMensagem.text = "Escolha o capitao da sua equipe"; // muda o texto visivel para o jogador
+
+            // quando o jogador apertar Enter a carta que instanciada é selecionada para ser o capitao
             if (Input.GetKeyDown(KeyCode.Return))
             {
                 time.MontaCapitao(Inventory.playerData.inventarioCartas[inventarioManager.i], ScriptableObject.CreateInstance<Equipamento>());
-                recebeuCarta = true;
+                recebeuCarta = true; // ao colocar que recebeu carta como true o jogador pode sair do while
 
                 FindObjectOfType<AudioManager>().Play("Select Carta");
             }
             yield return null;
         }
-        recebeuCarta = false;
+        recebeuCarta = false; // agora é necessario colocar recebeu carta como falso para o jogador ficar novamente preso na selecao
 
-
+        // Enquanto o jogador nao escolher uma carta ele fica preso na selecao de membros
         while (!recebeuCarta)
         {
-            textoMensagem.text = "Escolha o membro " + i + " da sua equipe";
-            // Sempre que apertar Space manda uma carta para o formador de membro, até que sejam mandados os ultimos 3 membros da equipe
+            textoMensagem.text = "Escolha o membro " + i + " da sua equipe"; // muda o texto visivel para o jogador
+
+            // Sempre que apertar Enter manda uma carta para o formador de membro, até que sejam mandados os ultimos 3 membros da equipe
             if (Input.GetKeyDown(KeyCode.Return))
             {
                 time.MontaMembro(Inventory.playerData.inventarioCartas[inventarioManager.i], ScriptableObject.CreateInstance<Equipamento>());
@@ -237,9 +190,10 @@ public class TorreManager : MonoBehaviour
             }
             yield return null;
         }
-        textoMensagem.text = "Time Formado!";
-        yield return new WaitForSecondsRealtime(1);
-        caixaMensagem.SetActive(false);
+
+        textoMensagem.text = "Time Formado!"; // muda o texto visivel para o jogador
+        yield return new WaitForSecondsRealtime(1); // espera um segundo para o jogador poder ler a mensagem de time formado
+        caixaMensagem.SetActive(false); // caixa de mensagem nao sera mais necessaria por isso ela é desativada
 
         // Com essa atribuicao de false, as setas para direita e esquerda nao instanciam mais novas cartas
         gameObject.GetComponent<TorreManager>().inventarioManager.mostrandoInventario = false;
@@ -247,40 +201,120 @@ public class TorreManager : MonoBehaviour
         // Destroy a ultima carta instanciada
         Destroy(GameObject.Find("Carta"));
 
+        // Chama a funcao que efetivamente formará o time
         time.MontaTime();
 
-        //SelecionaTorre();
-
-        IniciaTorre(); // Transferir essa chamada pra SelecionaTorre()
+        // Inicia de fato a torre
+        IniciaTorre();
 
         yield break;
     }
 
+    // Comeca a torre de fato
     public void IniciaTorre()
     {
-        torreEmAndamento = true;
-        andar = 0;
-        inimigoUI.SetActive(true);
-        playerUI.SetActive(true);
 
+        torreEmAndamento = true;    // define que o jogador já está jogando a torre
+        andar = 0;                  // define que o jogador está no primeiro andar da torre
+        inimigoUI.SetActive(true);  // ativa a UI do inimigo
+        playerUI.SetActive(true);   // ativa a UI do jogador
+
+        // inicia um som referente ao inicio da torre
         FindObjectOfType<AudioManager>().Play("Comeco Torre");
 
+        // inicia a musica tema da torre (Salvo no Scriptable Object da torre e não em um Game Object)
         gameObject.GetComponent<AudioSource>().clip = torre.musicaTema;
         gameObject.GetComponent<AudioSource>().Play();
 
-        //PLAYER
+        // PLAYER
         // Prepara Stats Player
         vidaPlayer = time.poderT; // A vida do player é equivalente ao poder total do time
 
-        //INIMIGO
+        // INIMIGO
+        // Carrega os Stats do Inimigo
         CarregaInimigo();
 
     }
-    public void SelecionaTorre(Torre torre)
+
+    // Termina a torre onde o jogador perdeu
+    public IEnumerator GameOver()
     {
-        this.torre = torre;
+        Debug.Log("Voce Perdeu!"); // Debugado para ajudar na criacao do codigo
+
+        // para a musica tema e comeca a musica de derrota
+        gameObject.GetComponent<AudioSource>().Stop();
+        FindObjectOfType<AudioManager>().Play("Lose Theme");
+
+        // ativa a caixa de mensagem novamente e coloca uma mensagem de derrota, tambem coloca a caixa no centro da tela
+        caixaMensagem.SetActive(true);
+        caixaMensagem.transform.position = GameObject.Find("Canvas").transform.position;
+        textoMensagem.text = "Voce Perdeu! Volte Tudo";
+        
+        // define que a torre parou
+        torreEmAndamento = false;
+
+        // Pausa o jogo até que o jogador clique alguma tecla, assim ele pode ouvir Sadness and Sorrow
+        bool continueGame = false;
+        while (!continueGame)
+        {
+            if (Input.anyKeyDown)
+            {
+                continueGame = true;
+                FindObjectOfType<AudioManager>().Stop("Lose Theme");
+            }
+
+            yield return null;
+        }
+
+        // volta o jogador para o menu
+        MenuManager.LoadMenuScene();
+    }
+   
+    // Termina a torre e atribui recompensas ao jogador
+    public IEnumerator Vitoria()
+    {
+        Debug.Log("Parabens Voce Completou a Torre"); // Debugado para ajudar na criacao do codigo
+
+        // para a musica tema e comeca a musica de vitoria
+        gameObject.GetComponent<AudioSource>().Stop();
+        FindObjectOfType<AudioManager>().Play("Vitoria Theme");
+
+        // ativa a caixa de mensagem novamente e coloca uma mensagem de derrota, tambem coloca a caixa no centro da tela
+        caixaMensagem.SetActive(true);
+        caixaMensagem.transform.position = GameObject.Find("Canvas").transform.position;
+        textoMensagem.text = "Parabens, Voce Completou a Torre!";
+
+        // define que a torre parou
+        torreEmAndamento = false;
+
+        // Recompensa o jogador por ter terminado a torre
+        RecompensaJogador();
+
+        // Pausa o jogo até que o jogador clique alguma tecla, assim ele pode ouvir Victory Fanfare
+        bool continueGame = false;
+        while (!continueGame)
+        {
+            if (Input.anyKeyDown)
+            {
+                continueGame = true;
+                FindObjectOfType<AudioManager>().Stop("Vitoria Theme");
+            }
+
+            yield return null;
+        }
+
+        // volta o jogador para o menu
+        MenuManager.LoadMenuScene();
     }
 
+    // Recompensa o jogador por ter terminado a torre, essa funcao é unutil na forma atual do codigo, porem caso o sistema de recompensa mude 
+    // ou algo do genero, essa funcao será o intermediario
+    public void RecompensaJogador()
+    {
+        GameManager.AplicaRecompensa(torre.recompensa);
+    }
+
+    // Passa o jogador para o proximo andar da torre
     public void ProximoAndar()
     {
         // Da experiencia para as cartas que participaram da batalha
@@ -310,6 +344,7 @@ public class TorreManager : MonoBehaviour
         CarregaInimigo();
     }
 
+    // Carrega os Stats do inimigo
     public void CarregaInimigo()
     {
         // Prepara Stats do inimigo
@@ -323,24 +358,32 @@ public class TorreManager : MonoBehaviour
         AtualizaVidaUI(); // Atualiza a vida do Player e do inimigo
     }
 
+    // Atualiza do jogador e a do Inimigo ao final do turno
     public void AtualizaVidaUI() // Essa funcao apenas atualiza a vida dos personagens da torre, ela deve ser chamada toda vez que um turno passar
     {
         vidaInimigoUI.text = "Vida: " + vidaInimigo.ToString() + "/" + inimigo.vida;
         vidaPlayerUI.text = "Vida: " + vidaPlayer.ToString() + "/" + time.poderT;
     }
 
+    // Aplica uma acao, essa funcao aplica tanto ataque quanto cura
     public void AplicaAcao(int APlayer, int Ainimigo) // Aplica as acoes feitas pelas dois entidades.
     {
         if (APlayer > 0)   // Player Efetuou um ataque
         {
+            // Toca o som de ataque, se estiver rolando um som de ataque ou cura ele para, o de ataque para sem o Stop, pois é o mesmo som que sera novamente chamado
             FindObjectOfType<AudioManager>().Stop("Cura Sound");
             FindObjectOfType<AudioManager>().Play("Ataque Sound");
+
+            // Efetua o ataque
             vidaInimigo -= APlayer;
         }
         else               // Player Efetuou uma cura, como o valor sera negativo voce subtrai ele da vida do player, assim faz uma soma 
         {
+            // Toca o som de cura, se estiver rolando um som de ataque ou cura ele para, o de cura para sem o Stop, pois é o mesmo som que sera novamente chamado
             FindObjectOfType<AudioManager>().Stop("Ataque Sound");
             FindObjectOfType<AudioManager>().Play("Cura Sound");
+
+            // Efetua a cura
             vidaPlayer -= APlayer;
         }
 
@@ -349,9 +392,11 @@ public class TorreManager : MonoBehaviour
         else               // Inimigo Efetuou uma cura
             vidaInimigo -= Ainimigo;
 
+        // impede que a cura seja maior que a vida maxima do jogador
         if (vidaPlayer > time.poderT)
             vidaPlayer = time.poderT;
 
+        // Atualiza a vida do jogador e do inimigo
         AtualizaVidaUI();
     }
 
